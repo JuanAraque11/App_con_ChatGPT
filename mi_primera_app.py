65791322a2f27a615b0f96b4fe8aece9ed33dc1f
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-02-17 13:59:08 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -6,8 +6,8 @@ st.write("Esta app fue elaborada por Juan José Echavarria Araque.")
 
 # Preguntar el nombre al usuario
 nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
-peso_usuario = st.text_input("Por favor, ingresa tu peso")
+peso_usuario = st.text_input("Por favor, ingresa tu peso en kg")
 
 # Imprimir mensaje de bienvenida
 if nombre_usuario and peso_usuario:
-    st.write(f"hola {peso_usuario}, tu peso es {nombre_usuario}.")
+    st.write(f"hola {peso_usuario} kg, tu peso es {nombre_usuario}.")
