17076d02193dc55c23889e30a579422533fb7243
JuanAraque11 <111087169+JuanAraque11@users.noreply.github.com>
2024-02-17 13:57:26 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
 # Título y autor
-st.title("Mi primera app")
+st.title("Mi app")
 st.write("Esta app fue elaborada por Juan José Echavarria Araque.")
 
 # Preguntar el nombre al usuario
 nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+peso_usuario = st.text_input("Por favor, ingresa tu peso")
 
 # Imprimir mensaje de bienvenida
-if nombre_usuario:
-    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
+if nombre_usuario and peso_usuario:
+    st.write(f"hola {peso_usuario}, tu peso es {nombre_usuario}.")
